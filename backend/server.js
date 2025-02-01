797e9b22a263244f16f2c40be7f7fd82c346f4b3
Naga Manikanta <maniking22000@gmail.com>
2025-02-01 20:10:37 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import express from "express";
 import authRoutes from "./routes/app.route.js"
 import connectToMongoDb from "./db/connectToMongoDb.js";
 import userRoutes from "./routes/user.route.js";
+import path from "path";
 
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./socket/socket.js";
@@ -13,19 +14,28 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000 ;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser())
-
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname , "/frontend/dist")));
+
+
 
 
 
 // app.get("/",(req,res)=>{
 //   res.send("Hello World");
 // })
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
 
 server.listen(PORT,()=>{
   connectToMongoDb();
